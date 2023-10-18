a859f312e98b0921509b0077692fd25ad9469d62
kanto <kantothier@gmail;com>
2023-10-18 17:25:56 +0200

    Mise à jour Horloge.java

--- src/horloge/Horloge.java
+++ src/horloge/Horloge.java
@@ -6,15 +6,26 @@ import strategie.StrategieAffichage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Horloge implements Sujet {
     private List<observateur> observateurs;
     private StrategieAffichage strategieAffichage;
+    private Timer timer;
+    private RecuperateurHeure recuperateur;
+    private StockageHeure stockageHeure;
+    private long intervalleSynchronisation;
 
     public Horloge() {
         this.observateurs = new ArrayList<>();
         // Initialiser la stratégie d'affichage par défaut
         this.strategieAffichage = new AffichageFormatUniversel();
+        this.timer = new Timer();
+        this.recuperateur = new RecuperateurHeure();
+        this.stockageHeure = new StockageHeure();
+        this.intervalleSynchronisation = 3600000; // 1 heure en millisecondes (1000 ms * 60 s * 60 min)
+        planifierSynchronisationPeriodique();
     }
 
     public void setStrategieAffichage(StrategieAffichage strategie) {
@@ -40,10 +51,14 @@ public class Horloge implements Sujet {
 
     public void synchroniser() {
         // Logique de synchronisation avec la source externe
-        // Met à jour les heures, minutes et secondes
-        // ...
+        int heures = recuperateur.recupererHeure();
+        int minutes = recuperateur.recupererMinutes();
+        int secondes = recuperateur.recupererSecondes();
 
-        // Après la synchronisation, notifier les observateurs
+        // Mettre à jour les heures, minutes et secondes dans le StockageHeure
+        stockageHeure.mettreHeure(heures, minutes, secondes);
+
+        // Notifier les observateurs après la synchronisation
         notifierObservateurs();
     }
 
@@ -57,8 +72,12 @@ public class Horloge implements Sujet {
         strategieAffichage.afficher(heures, minutes, secondes);
     }
 
-    public void mettreAJourHeure() {
-        // Logique pour mettre à jour l'heure
-        // ...
+    private void planifierSynchronisationPeriodique() {
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                synchroniser();
+            }
+        }, 0, intervalleSynchronisation);
     }
 }
